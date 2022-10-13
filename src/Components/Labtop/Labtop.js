990655c7eba694c0d807f1e40374f4bca935436b
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-10-13 23:01:05 +0600

    Modify

--- src/Components/Labtop/Labtop.js
+++ src/Components/Labtop/Labtop.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { sub } from '../Utilities/Utilities';
 
 const Labtop = () => {
+    const first = 20;
+    const second = 10;
+    const resultSub = sub(first,second)
     return (
         <div>
             <h3>This is Labtop Componenet</h3>
+            <p>Total Sub:{resultSub}</p>
         </div>
     );
 };
--- src/Components/Mobile/Mobile.js
+++ src/Components/Mobile/Mobile.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { add } from '../Utilities/Utilities';
 
 const Mobile = () => {
+    const first = 10;
+    const second = 5;
+    const resultSum = add(first,second)
     return (
         <div>
             <h3>This is Mobile Componenet</h3>
+            <p>Total Sum: {resultSum}</p>
         </div>
     );
 };
